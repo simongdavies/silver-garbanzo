0f5613d27bd10f4d88367d0957d20ca573f9bb9d
Simon Davies <simongdavies@users.noreply.github.com>
2019-05-10 15:06:25 +0100

    updating duffle location

--- duffle/build_bundle.sh
+++ duffle/build_bundle.sh
@@ -4,18 +4,19 @@ set -e
 
 echo "Download Duffle"
 
+mkdir ${agent_temp_directory}/duffle
 duffle_version="/0.1.0-ralpha.5%2Benglishrose"
-curl https://github.com/deislabs/duffle/releases/download/${duffle_version}/duffle-linux-amd64 -L -o  $(Agent.WorkFolder)/duffle
-chmod +x $(Agent.WorkFolder)/duffle
+curl https://github.com/deislabs/duffle/releases/download/${duffle_version}/duffle-linux-amd64 -L -o  ${agent_temp_directory}/duffle/duffle
+chmod +x ${agent_temp_directory}/duffle
+export PATH=${agent_temp_directory}/duffle:${PATH}
 
 echo "Get the files in the PR to find the solution folder name"
 
 # Each bundle definition should exist with a directory under the duffle directory - the folder name is derived from the set of files that have been changed in this pull request
 
 folder=$(curl "https://api.github.com/repos/$(Build.Repository.Name)/pulls/$(System.PullRequest.PullRequestNumber)/files")|jq [.[].filename| select(startswith("duffle"))][0]|split("/")[1]
-
 echo "Building Bundle in Solution Directory: $(Build.Repository.LocalPath)/duffle/${folder}"
-
 cd $(Build.Repository.LocalPath)/duffle/${folder}
-$(Agent.WorkFolder)/duffle init
-$(Agent.WorkFolder)/duffle build
+
+duffle init
+duffle build
