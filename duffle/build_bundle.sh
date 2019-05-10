f38c7ebc78bf57df0c82657d34cef0fc809e0fb7
Simon Davies <simongdavies@users.noreply.github.com>
2019-05-10 15:18:59 +0100

    updated variables

--- duffle/build_bundle.sh
+++ duffle/build_bundle.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 set -e 
+duffle_version="/0.1.0-ralpha.5%2Benglishrose"
 
 echo "Download Duffle"
 
 mkdir ${agent_temp_directory}/duffle
-duffle_version="/0.1.0-ralpha.5%2Benglishrose"
 curl https://github.com/deislabs/duffle/releases/download/${duffle_version}/duffle-linux-amd64 -L -o  ${agent_temp_directory}/duffle/duffle
 chmod +x ${agent_temp_directory}/duffle
 export PATH=${agent_temp_directory}/duffle:${PATH}
@@ -14,9 +14,9 @@ echo "Get the files in the PR to find the solution folder name"
 
 # Each bundle definition should exist with a directory under the duffle directory - the folder name is derived from the set of files that have been changed in this pull request
 
-folder=$(curl "https://api.github.com/repos/$(Build.Repository.Name)/pulls/$(System.PullRequest.PullRequestNumber)/files"|jq '[.[].filename| select(startswith("duffle"))][0]|split("/")[1]' --raw-output) 
-echo "Building Bundle in Solution Directory: $(Build.Repository.LocalPath)/duffle/${folder}"
-cd $(Build.Repository.LocalPath)/duffle/${folder}
+folder=$(curl "https://api.github.com/repos/${repo_name}/pulls/${pr_number}/files"|jq '[.[].filename| select(startswith("duffle"))][0]|split("/")[1]' --raw-output) 
+echo "Building Bundle in Solution Directory: ${repo_local_path}/duffle/${folder}"
+cd "${repo_local_path}/duffle/${folder}"
 
 duffle init
 duffle build
