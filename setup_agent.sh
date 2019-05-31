2bfe4f15e9d484131becb489feba65dfaf4b2116
Simon Davies <simongdavies@users.noreply.github.com>
2019-05-31 16:33:39 +0100

    changes to cred anmes

--- setup_agent.sh
+++ setup_agent.sh
@@ -34,10 +34,10 @@ printf "tool:%s\\n" "${tool}"
 # Each bundle definition should exist with a directory under the duffle directory - the folder name is derived from the set of files that have been changed in this pull request
 
 if [ "${tool}" ]; then
-    if [ "$(find "${repo_local_path}/${tool}" -maxdepth 1 ! -type d)" ]; then 
-        printf "Files should not be placed in the %s directory - only %s solution folders in this folder. \\n" "${tool}" "${tool}"
-        exit 1 
-    fi
+    # if [ "$(find "${repo_local_path}/${tool}" -maxdepth 1 ! -type d)" ]; then 
+    #     printf "Files should not be placed in the %s directory - only %s solution folders in this folder. \\n" "${tool}" "${tool}"
+    #     exit 1 
+    # fi
     folder=$(echo "${files}"|jq --arg tool "${tool}" '.|map(select(startswith($tool)))[0]|split("/")[1]' --raw-output)
     echo "##vso[task.setvariable variable=tool]${tool}"
 fi
